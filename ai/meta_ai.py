933be7a9445eab0fa3c5ee4310f9b439a117e44b
Ujjawal <ujjawalupadhyay2004@gmail.com>
2025-02-13 12:49:06 +0530

    Changed Meta to Gemini

--- ai/meta_ai.py
+++ ai/meta_ai.py
@@ -1,13 +1,41 @@
-from meta_ai_api import MetaAI
+# from meta_ai_api import MetaAI
+
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-ai_instance = MetaAI()
+# ai_instance = MetaAI()
 
-prebuilt_message = "You have to act like a cute discord bot named Akio. Only when asked, say your name is Akio, don't state your name out of the blue. Now the user will give a query: "
+prebuilt_message = "You have to act like a cute discord bot named Akio. Only when asked, say your name is Akio, don't state your name out of the blue and don't use emojis, you can use emoticons but not too frequently. Now the user will give a query: "
 conversation_history = prebuilt_message  # Initialize with the prebuilt message
+
+
+import os
+import google.generativeai as genai
+
+genai.configure(api_key="AIzaSyB1S8rGxGv9E7wQ1Hu1V7iss3F13Zydr8Q")
+
+# Create the model
+generation_config = {
+  "temperature": 0.7,
+  "top_p": 0.95,
+  "top_k": 64,
+  "max_output_tokens": 65536,
+  "response_mime_type": "text/plain",
+}
+
+model = genai.GenerativeModel(
+  model_name="gemini-2.0-flash-thinking-exp-01-21",
+  generation_config=generation_config,
+  system_instruction=prebuilt_message
+)
+
+chat_session = model.start_chat(
+  history=[
+  ]
+)
+# print(response.text)
 
 async def ai(user_input: str) -> str:
     global conversation_history  # Access the conversation history
@@ -18,7 +46,9 @@ async def ai(user_input: str) -> str:
         
         print(f"Debug: Sending prompt -> {conversation_history}")
         # Send the full conversation history as a prompt to retain context
-        response = ai_instance.prompt(message=conversation_history)
+
+        response = chat_session.send_message(user_input)
+        response = response.text.strip()
         print(f"Debug: Received response -> {response}")  # Add this to log API output
         # Process the response
         if isinstance(response, dict) and 'message' in response:
@@ -36,4 +66,5 @@ async def ai(user_input: str) -> str:
         return "Sorry, I couldn't generate a response."
     
     except Exception as e:
-        return f"An error occurred: {e}"
+        print(f"An error occurred: {e}")
+        return ""
