d5e22444e3ed2d2b5c321eea59f94072c5812cbf
Cyphernk13 <ujjawalupadhyay2004@gmail.com>
2025-04-22 07:34:02 +0530

    status changed

--- scripts/setup.py
+++ scripts/setup.py
@@ -9,6 +9,6 @@ bot = commands.Bot(command_prefix=['Akio ', 'akio '], help_command=None, intents
 
 @bot.event
 async def on_ready():
-    await bot.change_presence(activity=discord.Streaming(name="My Stream", url="https://www.twitch.tv/discord"))
+    await bot.change_presence(activity=discord.Streaming(name="akio help", url="https://www.twitch.tv/discord"))
     print(f'Logged in as {bot.user.name}')
     await bot.tree.sync()
